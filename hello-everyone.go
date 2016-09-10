176006a5ce9f9f518c1efb3dec15928a0ce90b06
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2016-09-10 14:23:22 -0700

    minor change to test badge

--- hello-everyone.go
+++ hello-everyone.go
@@ -15,6 +15,6 @@ func main() {
 	var a = 3
 	var b = 2
 	var sum = addthis(a, b)
-	fmt.Println("Hello, everyone. How are you", sum)
+	fmt.Println("Hello, everyone. How are you today", sum)
 	fmt.Println(os.Getenv("GOPATH"))
 }
