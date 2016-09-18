484d26a19aa64f8430ca179103f83f00a50e5887
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2016-09-18 04:04:53 -0700

    edited script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,14 +3,23 @@
 
 set -e -x
 
-#The code is located in /hello-go
+# The code is located in /hello-go
 echo "List whats in the current directory"
 ls -lat 
 
-# Enter the directory
-cd hello-go
+# Setup the gopath based on current directory.
+export GOPATH=$PWD
 
-# put the binary hello-go filename in /dist
+# Now we must move our code from the current directory ./hello-go to $GOPATH/src/github.com/JeffDeCola/hello-go
+mkdir -p src/github.com/JeffDeCola/
+cp -R ./hello-go src/github.com/JeffDeCola/.
+
+# All set and everything is in the right place for go
+echo "Gopath is: " $GOPATH
+echo "pwd is: " $PWD
+cd src/github.com/JeffDeCola/hello-go
+
+# Put the binary hello-go filename in /dist
 go build -o dist/hello-go ./main.go
 
 # cp the Dockerfile into /dist
