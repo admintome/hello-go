834a40d7b211b0c9fcb980b230566fd983fc3f53
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2016-09-20 04:53:11 -0700

    updated pic and pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -6,6 +6,7 @@ set -e -x
 # The code is located in /hello-go
 echo "List whats in the current directory"
 ls -lat 
+echo ""
 
 # Setup the gopath based on current directory.
 export GOPATH=$PWD
@@ -17,6 +18,7 @@ cp -R ./hello-go src/github.com/JeffDeCola/.
 # All set and everything is in the right place for go
 echo "Gopath is: " $GOPATH
 echo "pwd is: " $PWD
+echo ""
 cd src/github.com/JeffDeCola/hello-go
 
 # Put the binary hello-go filename in /dist
@@ -28,12 +30,19 @@ cp ci/Dockerfile dist/Dockerfile
 # Check
 echo "List whats in the /dist directory"
 ls -lat dist
+echo ""
 
-# Move what you need to $GOPATH - BECAUSE THIS IS WHERE the resource type docker-image works. 
-# Not really ideal, but it works. 
+# Move what you need to $GOPATH/dist
+# BECAUSE the resource type docker-image works in /dist.
 cp -R ./dist $GOPATH/.
 
 cd $GOPATH
 # Check whats here
 echo "List whats in top directory"
 ls -lat 
+echo ""
+
+# Check whats in /dist
+echo "List whats in /dist"
+ls -lat dist
+echo ""
