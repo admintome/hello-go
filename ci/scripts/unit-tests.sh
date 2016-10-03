4e4d2d58438a3a25c8f1cc85fe3295e74402aadd
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2016-10-03 13:13:06 -0700

    added some whitespace to test_coverage.txt

--- ci/scripts/unit-tests.sh
+++ ci/scripts/unit-tests.sh
@@ -22,7 +22,9 @@ cd src/github.com/JeffDeCola/hello-go
 
 # RUN unit_tests and it shows the percentage coverage
 # print to stdout and file using tee
-go test -v -cover ./... | tee test_coverage.txt
+go test -cover ./... | tee test_coverage.txt
+# add some whitespace to the begining of each line
+sed -i -e 's/^/     /' test_coverage.txt
 
 # Move to coverage-results directory.
 mv test_coverage.txt $GOPATH/coverage-results/.
