5a96c087fcb5a1e9dda10e8a3aef732a43c785aa
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2016-10-10 10:25:14 -0700

    test

--- ci/scripts/readme-github-pages.sh
+++ ci/scripts/readme-github-pages.sh
@@ -8,11 +8,18 @@ echo "pwd is: " $PWD
 echo "List whats in the current directory"
 ls -lat 
 
+cp -r hello-go hello-go-updated
+
+cd hello-go-updated
+
 # FOR GITHUB WEBPAGES
 # BASICALLY COPY README.md to /docs/_includes/README.md
 # Remove everything before the second hedading.
-sed '0,/GitHub Webpage/d' hello-go/README.md > hello-go/docs/_includes/README.md
-cd hello-go
+sed '0,/GitHub Webpage/d' README.md > docs/_includes/README.md
+
+#ADD AND COMMIT
+git config --global user.email "jeff@kepperlabs.com"
+git config --global user.name "Jeff DeCola"
 
 git add .
 git commit -m "COPY README.md to /docs/_includes/README.md"
