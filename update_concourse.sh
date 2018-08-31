68994d71a67624b7a90d1ff5da2998ff5297b581
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2018-08-30 17:45:59 -0700

    udpated concourse

--- update_concourse.sh
+++ update_concourse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # hello-go update_concourse.sh
 
-fly -t ci set-pipeline -p hello-go -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
+fly -t ci set-pipeline -p hello-go -c ci/pipeline.yml --load-vars-from ../.credentials.yml
